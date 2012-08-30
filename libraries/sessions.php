f9f102089975955bddeaef0d8b177efa10f1b45f
whiskeyman <WhiskeyMan.Tau@gmail.com>
2012-08-30 13:15:29 +0400

    Обработчик сессий

--- libraries/sessions.php
+++ libraries/sessions.php
@@ -1,10 +1,12 @@
 <?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: whiskeyman
+ * Date: 30.08.12
+ * Time: 13:24
+ * To change this template use File | Settings | File Templates.
+ */
+class TSessions
+{
 
-defined('_TEXEC') or die;
-
-class TSessions{
-
-    public function __comstruct()
-
-    }
-?>
+}
