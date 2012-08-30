ccdf978a310eefc7317874a817130d1ca296b45f
whiskeyman <WhiskeyMan.Tau@gmail.com>
2012-08-30 16:08:53 +0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	libraries/sessions.php

---
+++ libraries/session.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: whiskeyman
+ * Date: 30.08.12
+ * Time: 16:08
+ * To change this template use File | Settings | File Templates.
+ */
+class TSession
+{
+
+}
