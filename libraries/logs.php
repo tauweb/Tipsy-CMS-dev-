83c066452289b263c4e87d9a7420248cdfecec8f
whiskeyman <WhiskeyMan.Tau@gmail.com>
2012-08-31 23:30:57 +0400

    Добавлен класс TLogs для логирования происходящего

---
+++ libraries/logs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: whiskeyman
+ * Date: 31.08.12
+ * Time: 23:30
+ * To change this template use File | Settings | File Templates.
+ */
+class TLogs
+{
+
+}
