af2b4261a5584e9d0e26f29889a482f2c32124f0
whiskeyman <WhiskeyMan.Tau@gmail.com>
2012-08-31 16:16:19 +0400

    Добавлен класс TQuery

--- libraries/_database/mysqli.php
+++ libraries/_database/mysqli.php
@@ -43,6 +43,4 @@ class TMysqli extends mysqli
     }
 
 }
-
-
 ?>
---
+++ libraries/_database/query.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: whiskeyman
+ * Date: 31.08.12
+ * Time: 16:15
+ * To change this template use File | Settings | File Templates.
+ */
+class TQuery
+{
+
+}
