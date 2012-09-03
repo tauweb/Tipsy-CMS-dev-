058d1b60027e718ff864f14d3b6f0782a56a018a
whiskeyman <WhiskeyMan.Tau@gmail.com>
2012-09-03 23:32:27 +0400

    Изменены настройки базы данных

--- configs/config.php
+++ configs/config.php
@@ -5,8 +5,8 @@ defined('_TEXEC') or die;
 // Параметры конфигурации системы
 abstract class TConfig
 {
-
 	// Параметры подключения к базе данных
+//Todo: Правильная база whiskeyman_tipsy, параметры базы данных изменены на неверные для проверки модулей логирования и исключения
 	public static $db_type = 'mysqli';
 	public static $db_host = 'localhost';
 	public static $db_server_port = '';
