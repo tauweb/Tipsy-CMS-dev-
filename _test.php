88a53a60fd48a684336b3e7192d6baafc33b8f72
whiskeyman <WhiskeyMan.Tau@gmail.com>
2012-10-11 17:13:36 +0400

    Для тестов

---
+++ _test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: whiskeyman
+ * Date: 04.09.12
+ *
+ */
+$con = new mysqli('localhost', 'whiskeyman_tipsy', 'password', 'whiskeyman_tipsy');
+
+		$QueryStr = 'SELECT * FROM articles;' ;
+		
+		$res = $con->query($QueryStr);
+		// отладочная часть
+		var_dump($res);
+		while($row=mysql_fetch_array($res))
+			echo $row;
+
+
+?>
