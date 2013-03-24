2aeabad59a8780c4869f5d6a69f5a7cdc6d51070
whiskeyman <WhiskeyMan.tau@gmail.com>
2013-03-25 01:29:53 +0400

    Доработка кода

--- index.php
+++ index.php
@@ -11,6 +11,23 @@ namespace Tipsy;
 // Устанавливает что это главный файл.
 define('_TEXEC', 1);
 
+// Установки php.ini
+ini_set('display_errors', 'on');
+
+ini_set('session.use_cookies', 'On');
+ini_set('session.use_trans_sid', 'Off');
+session_set_cookie_params(0, '/');
+
+// Устанавливаю отображение ошибок php
+error_reporting(E_CORE_ERROR | E_CORE_WARNING
+    | E_COMPILE_ERROR | E_ERROR
+    | E_WARNING | E_PARSE
+    | E_USER_ERROR | E_USER_WARNING
+    | E_RECOVERABLE_ERROR);
+// Отображать все ошибки и предупреждения (убрать, если задан пользовательский обработчик ошибок)
+error_reporting(E_ALL);
+
+
 // Проверяет версию php. Для работы системы нужна >= 5.4.
 phpversion() < 5.4 ? die('<b>ВНИМАНИЕ!</b> Данная версия php не поддерживается.
 					Для работы системы нужна версия >= 5.4') : '';		// Todo: Позже вынести в отдельный модуль!!!
