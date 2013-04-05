3fd7515d28716eaff7736b5efabe380982d34c44
whiskeyman <whiskeyman.tau@gmail.com>
2013-04-05 12:38:51 +0400

    Все только начинается...

---
+++ Components/Articles/Articles.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: whiskeyman
+ * Date: 05.04.13
+ * Time: 12:38
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Articles;
+
+
+class Articles {
+
+}
