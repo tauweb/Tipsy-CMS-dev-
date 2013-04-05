0d10cb544c9b85b950ec8bf26f879c0066ab6db6
whiskeyman <whiskeyman.tau@gmail.com>
2013-04-05 13:06:27 +0400

    Все только начинается...

--- Components/Articles/Articles.php
+++ Components/Articles/Articles.php
@@ -1,15 +1,38 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
+ * Классотвечающий за вывод и создание статей в системе..
  * User: whiskeyman
  * Date: 05.04.13
  * Time: 12:38
- * To change this template use File | Settings | File Templates.
  */
 
-namespace Articles;
+namespace Tipsy\Components\Articles;
 
+use Tipsy\Libraries\Database\Query;
 
-class Articles {
+abstract class Articles {
 
+	public static function init(){
+		echo 'test';
+	}
+
+	public static function getArticle($param, $id)
+	{
+		$queryParam = $param == '*' ? $param  : '`' . $param . '`';
+		// Формирует строку запроса
+		$sql = "SELECT $queryParam FROM `articles` WHERE `articleid` = $id;";
+
+		$row = Query::select($sql);
+		// Эта часть будет нужа для последующего формирования таблицы вывода, когда, например будут выбираться все поля
+
+		if($param == 'all' or $param == '*'){
+			foreach (array_keys($row) as $colName){
+				echo $colName . '<p>';
+			}
+		}else{
+			// Это обычный вывод
+			echo  $row[$param];
+		}
+
+	}
 }
