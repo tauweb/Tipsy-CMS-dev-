a40506dfd7b427b4860fda3d4b8ba29d7a8134f4
whiskeyman <whiskeyman.tau@gmail.com>
2013-04-06 19:17:21 +0400

    Все только начинается...

--- DeveloperFiles/create_DB.sql
+++ DeveloperFiles/create_DB.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.3
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 26, 2013 at 08:04 AM
--- Server version: 5.1.67-andiunpam
--- PHP Version: 5.4.13
+-- Хост: localhost
+-- Время создания: Апр 06 2013 г., 19:15
+-- Версия сервера: 5.5.29
+-- Версия PHP: 5.4.13-2~precise+1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,72 +17,106 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `whiskeyman_tipsy`
+-- База данных: `whiskeyman_tipsy`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `articles`
+-- Структура таблицы `articles`
 --
 
 CREATE TABLE IF NOT EXISTS `articles` (
-`articleid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id статьи',
-`title` char(50) NOT NULL COMMENT 'название статьи',
-`fulltext` longtext NOT NULL COMMENT 'содержание статьи',
-`created` date NOT NULL COMMENT 'дата создания',
-`created_by` char(50) NOT NULL COMMENT 'кем создана',
-PRIMARY KEY (`articleid`)
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id статьи',
+  `title` char(50) NOT NULL COMMENT 'название статьи',
+  `fulltext` longtext NOT NULL COMMENT 'содержание статьи',
+  `created` date NOT NULL COMMENT 'дата создания',
+  `created_by` char(50) NOT NULL COMMENT 'кем создана',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Таблица статей' AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `articles`
+-- Дамп данных таблицы `articles`
 --
 
-INSERT INTO `articles` (`articleid`, `title`, `fulltext`, `created`, `created_by`) VALUES
+INSERT INTO `articles` (`id`, `title`, `fulltext`, `created`, `created_by`) VALUES
 (1, 'Описание', 'Tipsy cms находится в начальной стадии разработки. Так как она пишется практически вся на коленках в метро, параллельно изучению php, mySQL, html и css, сроки ее завершения совершенно не определены :)\n\nСайт создан для отлаживания исходников, когда я их пишу, например, в метро, когда другой возможности протестить работоспособность нет. А исходя из того, что, в основном, написание происходит в дороге с мобильного девайса, сиё творение будет чаще не работать, чем работать. ;)\n\nДля работы Tipsy CMS требуется php версии не ниже 5.4, html5 и css3<p>\n<b>Ведутся работы по переписанию кода, переход на использование простаранста имен.</b>', '2012-11-08', 'whiskeyman'),
 (2, 'заголовок', 'какой-то текст', '2012-11-04', 'whiskeyman');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `positions`
+-- Структура таблицы `components`
 --
 
-CREATE TABLE IF NOT EXISTS `positions` (
-`id` int(11) NOT NULL AUTO_INCREMENT COMMENT ' id позиции щаблона',
-`name` char(10) NOT NULL COMMENT 'Название позиции',
-PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Позиции шаблона' AUTO_INCREMENT=739 ;
+CREATE TABLE IF NOT EXISTS `components` (
+  `name` varchar(30) NOT NULL COMMENT 'Имя компонента системы',
+  `title` varchar(50) NOT NULL COMMENT 'Заголовок компонента',
+  `position_id` int(10) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Компоненты системы';
 
 --
--- Dumping data for table `positions`
+-- Дамп данных таблицы `components`
 --
 
-INSERT INTO `positions` (`id`, `name`) VALUES
-(174, 'Autorize');
+INSERT INTO `components` (`name`, `title`, `position_id`) VALUES
+('Article', '', 0),
+('User', '', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Структура таблицы `positions`
+--
+
+CREATE TABLE IF NOT EXISTS `positions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT ' id позиции щаблона',
+  `name` char(30) NOT NULL COMMENT 'Название позиции',
+  `com` char(30) NOT NULL COMMENT 'Компонент привязанный к позиции шаблона. Определяет какое содержимое будет выведено.',
+  `com_id` int(10) NOT NULL COMMENT 'id компонента',
+  `title` char(1) NOT NULL COMMENT 'Заголовок позиции',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Позиции шаблона' AUTO_INCREMENT=825 ;
+
+--
+-- Дамп данных таблицы `positions`
+--
+
+INSERT INTO `positions` (`id`, `name`, `com`, `com_id`, `title`) VALUES
+(824, 'footer', '', 0, ''),
+(823, 'article', 'Article', 1, ''),
+(822, 'nav', '', 0, ''),
+(821, 'conteiner', '', 0, ''),
+(820, 'menu_horisontal', '', 0, ''),
+(819, 'errors', '', 0, ''),
+(818, 'autorize', '', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL COMMENT 'id пользователя',
-`name` varchar(45) DEFAULT NULL COMMENT 'Имя пользователя',
-`username` varchar(45) DEFAULT NULL COMMENT 'Имя пользователя (логин)',
-`email` varchar(45) DEFAULT NULL COMMENT 'email позьзователя',
-`password` varchar(45) DEFAULT NULL COMMENT 'Пароль пользователя',
-`usertype` varchar(45) DEFAULT NULL COMMENT 'Тип пользователя',
-`registerDate` varchar(45) DEFAULT NULL COMMENT 'Дата регистрации',
-`lastvisitDate` varchar(45) DEFAULT NULL COMMENT 'Дата последнего визита',
-PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL COMMENT 'id пользователя',
+  `name` varchar(45) DEFAULT NULL COMMENT 'Имя пользователя',
+  `username` varchar(45) DEFAULT NULL COMMENT 'Имя пользователя (логин)',
+  `email` varchar(45) DEFAULT NULL COMMENT 'email позьзователя',
+  `password` varchar(45) DEFAULT NULL COMMENT 'Пароль пользователя',
+  `usertype` varchar(45) DEFAULT NULL COMMENT 'Тип пользователя',
+  `registerDate` varchar(45) DEFAULT NULL COMMENT 'Дата регистрации',
+  `lastvisitDate` varchar(45) DEFAULT NULL COMMENT 'Дата последнего визита',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Пользователи системы';
 
 --
--- Dumping data for table `users`
+-- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `usertype`, `registerDate`, `lastvisitDate`) VALUES
 (0, NULL, 'Whiskeyman', NULL, NULL, NULL, NULL, NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
