514f145c097579d710f005f1786d7f0d3ea5b4fe
whiskeyman <whiskeyman.tau@gmail.com>
2014-11-24 19:04:18 +0300

    Begining

--- Libraries/Loader.php
+++ Libraries/Loader.php
@@ -1,46 +1,50 @@
 <?php
 namespace Tipsy\Libraries;
+
 // Проверяет легален ли доступ к файлу
 defined('_TEXEC') or die;
 
 abstract class Loader
 {
-	/*
-	* @var sting Имя клааса откуда был вызван загрузчик используется для отладки
-	*/
-	public static $calledFrom ='';
-	public static $line;
+    /*
+    * @var sting Имя клааса откуда был вызван загрузчик используется для отладки
+    */
+    protected static $_calledFrom = '';
+    protected static $_line = '';
 
-	public static function loadClass($className, $calledFrom = '',$line =''){
-		if(class_exists($className)){
-			return true;
-		}
+    public static function loadClass($className, $_calledFrom = '', $_line = '')
+    {
+        if (class_exists($className)) {
+            return true;
+        }
 
-		if($calledFrom)
-			self::$calledFrom = $calledFrom;
-		if($line)
-			self::$line=$line;
+        if ($_calledFrom) {
+            self::$_calledFrom = $_calledFrom;
+        }
+        if ($_line) {
+            self::$_line = $_line;
+        }
 
-		$className = ltrim($className, '\\');
-		$fileName  = '';
-		$namespace = '';
-		if ($lastNsPos = strrpos($className, '\\')) {
-			$namespace = substr($className, 0, $lastNsPos);
-			$className = substr($className, $lastNsPos + 1);
-			$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-		}
-		$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-		
-		if(file_exists($fileName)){
-			require_once $fileName;
-			# todo отображать имя загружаемого файла (для отладки) echo $fileName."</br>";
-			return true;
-		}else{
-			#if(self::$calledFrom)
+        $className = ltrim($className, '\\');
+        $fileName = '';
+        $namespace = '';
+        if ($lastNsPos = strrpos($className, '\\')) {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-			echo "Загрузчик: не могу найти файл: <b>$fileName</b>, меня вызвал <b>"
-				. self::$calledFrom ."</b>, со строки <b>" . self::$line. "</b></b><p>";
-			return false;
-		}
-	}
+        if (file_exists($fileName)) {
+            require_once $fileName;
+            // todo отображать имя загружаемого// файла (для отладки) echo $fileName."</br>";
+            return true;
+        } else {
+            // if(self::$_calledFrom)
+
+            echo "Загрузчик: не могу найти файл: <b>$fileName</b>, меня вызвал <b>"
+                . self::$_calledFrom ."</b>, со строки <b>" . self::$_line. "</b><p>";
+            return false;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 namespace Tipsy;
 
 // Устанавливает что это главный файл.
+define('_TEXEC', 1);
+
 use Tipsy\Config\Config;
 
 /**
- * @package		Tipsy CMS
- * @version		0.0.1
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @author		Tkachenko Aleksey, e-mail: whiskeyman.tau@gmail.com
+ * @package     Tipsy CMS
+ * @version     0.0.1
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Tkachenko Aleksey, e-mail: whiskeyman.tau@gmail.com
  */
-define('_TEXEC', 1);
 
 echo '<meta charset="utf-8">'; // Для отладки вывода сообщений на русском
-
 
 // Подключаем настройки php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'php_set.php';
